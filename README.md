b25ae0a40c2d4e0218af3a185e09ca782a284527
gjee22 <G.Jee@student.tudelft.nl>
2024-06-25 14:14:01 +0200

    Adding sparse indexes, indexing.py (FF index creation), and additional README.md explanation

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # CSE3000-Research-Project-Neural-Ranking-Models
+The repository stores codebase for the implementation of experiments with rank fusion function in the Fast-Forward indexes setting.
+The framework takes the retrieve-and-rerank approach of neural ranking models with the following pipeline:
+1. Retrieve candidates using sparse scores.
+2. Compute dense scores of the retrieved candidates.
+3. Interpolate the two scores using a rank fusion function and obtain the final ranking of candidates.
+
+The implementation of the FF indexes is available in the following repository: https://github.com/mrjleo/fast-forward-indexes
+
 This repository saves the experiment code and data of the results 
 for the _Neural Ranking Models_ project for the 2024 CSE3000 Research Project.
 The documentations of the project can be found in https://github.com/TU-Delft-CSE/Research-Project?tab=readme-ov-file
+
+## Steps to Follow for Reproducing the Experiment
+1. Indexing: run indexing.py to create the FF indexes for each dataset
+2. Running the experiment: 
---
+++ Scidocs/indexing.py
@@ -0,0 +1,41 @@
+import pyterrier as pt
+from pathlib import Path
+from fast_forward.encoder import TCTColBERTQueryEncoder, TCTColBERTDocumentEncoder
+import torch
+from fast_forward import OnDiskIndex, Mode, Indexer
+
+
+def docs_iter(dataset):
+    """
+    Save the corpus data as a dictionary
+    :param dataset: dataset to index
+    :return: Dictionary of documents in the corpus
+    """
+    for d in dataset.get_corpus_iter():
+        yield {"doc_id": d["docno"], "text": d["text"], "title": d["title"]}
+
+
+def main():
+    """
+    Create Fast-Forward index for SCIDOCS
+    """
+    if not pt.started():
+        pt.init(tqdm="notebook")
+
+    dataset = pt.get_dataset("irds:beir/scidocs")
+
+    q_encoder = TCTColBERTQueryEncoder("castorini/tct_colbert-msmarco")
+    d_encoder = TCTColBERTDocumentEncoder(
+        "castorini/tct_colbert-msmarco",
+        device="cuda:0" if torch.cuda.is_available() else "cpu",
+    )
+
+    ff_index = OnDiskIndex(
+        Path("ffindex_scidocs_tct.h5"), dim=768, query_encoder=q_encoder, mode=Mode.MAXP, max_id_length=40
+    )
+
+    ff_indexer = Indexer(ff_index, d_encoder, batch_size=128)
+    ff_indexer.index_dicts(docs_iter(dataset))
+
+if __name__ == '__main__':
+    main()
